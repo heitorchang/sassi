516126d39dfcfa0851336f21d9c0a9f96c81314a
Heitor Chang <heitorchang@gmail.com>
2019-11-04 11:06:10 -0300

    allow local

--- sassi/settings.py
+++ sassi/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['sassi.nfshost.com']
+ALLOWED_HOSTS = ['sassi.nfshost.com', '127.0.0.1']
 
 
 # Application definition
