4ad946a4199000b8735fbe20a3b68b11fa8d96d6
Heitor Chang <heitorchang@gmail.com>
2019-11-05 12:07:45 -0300

    models

--- fridge/admin.py
+++ fridge/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
-# Register your models here.
+from . import models
+
+
+class RequirementInline(admin.StackedInline):
+    model = models.Requirement
+    extra = 4
+
+    
+class RecipeAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ("General Info", {'fields': ['name', 'procedure']}),
+    ]
+    inlines = [RequirementInline]
+
+    
+admin.site.register(models.Product)
+admin.site.register(models.Ingredient)
+admin.site.register(models.Fridge)
+admin.site.register(models.Requirement)
+admin.site.register(models.Recipe, RecipeAdmin)
--- fridge/models.py
+++ fridge/models.py
@@ -1,3 +1,75 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    """Abstract representation of (typically raw) food items
+    Keep names in lowercase singular.
+    cabbage, onion, rice
+    """
+
+    name = models.CharField(max_length=78)
+
+    
+    class Meta:
+        ordering = ['name']
+        
+    def __str__(self):
+        return self.name
+
+
+class Fridge(models.Model):
+    """State of refrigerator and other storage"""
+
+    name = models.CharField(max_length=31)
+
+    
+    class Meta:
+        ordering = ['name']
+    
+    def __str__(self):
+        return "{} ({} items)".format(self.name, self.ingredient_set.count())
+
+
+class Ingredient(models.Model):
+    """Specific amount, weight, etc. of a Product"""
+
+    fridge = models.ForeignKey(Fridge, on_delete=models.CASCADE, null=True, blank=True)  # belongs to
+    amount = models.CharField(max_length=78)  # units, weight or volume
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+
+
+    class Meta:
+        ordering = ['fridge', 'product', '-amount']
+        
+    def __str__(self):
+        return f"{self.amount} {self.product} ({self.fridge.name})"
+
+
+class Recipe(models.Model):
+    """Description of a recipe"""
+
+    name = models.CharField(max_length=78)
+    procedure = models.TextField(blank=True)
+
+    
+    class Meta:
+        ordering = ['name']
+        
+    def __str__(self):
+        return f"{self.name}"
+
+
+class Requirement(models.Model):
+    """Specific ingredient for a Recipe"""
+    
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    amount = models.CharField(max_length=78)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+
+    class Meta:
+        ordering = ['recipe', 'product', 'amount']
+
+    def __str__(self):
+        return f"{self.amount} {self.product} ({self.recipe.name})"
