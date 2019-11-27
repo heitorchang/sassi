d113bb7f39830fb1e2a483d2c4c22dff3833e344
Heitor Chang <heitorchang@gmail.com>
2019-11-27 16:28:22 -0300

    send to fridge

--- sassi/urls.py
+++ sassi/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 from ui.views import index as ui_index
+from fridge.views import index as fridge_index
 from django.views.generic.base import RedirectView
 
 urlpatterns = [
-    path('', RedirectView.as_view(url="/fridge/", permanent=False), name="index"),
+    # path('', RedirectView.as_view(url="/fridge/", permanent=False), name="index"),
+    path('', fridge_index, name="index"),
     path('menu/', ui_index, name="menu"),
     path('ui/', include('ui.urls')),
     path('admin/', admin.site.urls),
