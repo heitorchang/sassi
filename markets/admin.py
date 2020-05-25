a88f15d2d79e54866f9498c26d48890c0c598a47
Heitor Chang <heitorchang@gmail.com>
2020-05-25 18:09:07 -0300

    search prices

--- markets/admin.py
+++ markets/admin.py
@@ -1,4 +1,16 @@
 from django.contrib import admin
 from .models import Market, ProductCategory, Product, Brand, Price
 
-admin.site.register((Market, ProductCategory, Product, Brand, Price))
+class MarketAdmin(admin.ModelAdmin):
+    fields = ['name', 'address']
+
+
+class PriceAdmin(admin.ModelAdmin):
+    search_fields = ['product__name', 'market__name']
+    
+
+admin.site.register(Market, MarketAdmin)
+admin.site.register(ProductCategory)
+admin.site.register(Product)
+admin.site.register(Brand)
+admin.site.register(Price, PriceAdmin)
